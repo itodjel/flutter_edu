ffa866a44bfea663fb27abd7e4cfcbbc22f29b3c
luka <luka.jelacic@edu.fit.ba>
2021-03-24 10:49:00 +0100

    added counter_bloc

---
+++ challenges/logic/bloc_counter/luka_jelacic/lib/blocs/counter_bloc.dart
@@ -0,0 +1,27 @@
+import 'package:flutter_bloc/flutter_bloc.dart';
+import 'package:luka_jelacic/blocs/counter_event.dart';
+import 'package:luka_jelacic/blocs/counter_state.dart';
+
+class CounterBloc extends Bloc<CounterEvent, CounterState> {
+  CounterBloc() : super(CounterPressState(counter: 0));
+  @override
+  Stream<CounterState> mapEventToState(CounterEvent event) async* {
+    if (event is IncrementCounterEvent) {
+      yield* increment(event);
+    } else {
+      yield* decrement(event);
+    }
+  }
+
+  Stream<CounterState> incremment(IncrementCounterEvent event) async* {
+    yield LoadingState();
+    await Future.delayed(Duration(seconds: 1));
+    yield CounterPressState(counter: event.counter += 1);
+  }
+
+  Stream<CounterState> decrement(DecrementCounterEvent event) async* {
+    yield LoadingState();
+    await Future.delayed(Duration(seconds: 1));
+    yield CounterPressState(counter: event.counter -= 1);
+  }
+}
