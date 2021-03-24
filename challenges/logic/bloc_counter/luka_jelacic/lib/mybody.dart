17ee4b8667a4884247bbf5d42479b9880ae9d501
luka <luka.jelacic@edu.fit.ba>
2021-03-24 11:01:40 +0100

    added BlocBuilder in UI

--- challenges/logic/bloc_counter/luka_jelacic/lib/mybody.dart
+++ challenges/logic/bloc_counter/luka_jelacic/lib/mybody.dart
@@ -1,32 +1,61 @@
 import 'package:flutter/material.dart';
+import 'package:flutter_bloc/flutter_bloc.dart';
+import 'package:luka_jelacic/blocs/counter_bloc.dart';
+import 'package:luka_jelacic/blocs/counter_event.dart';
+import 'package:luka_jelacic/blocs/counter_state.dart';
 
 class MyBody extends StatelessWidget {
   @override
   Widget build(BuildContext context) {
+    return BlocBuilder<CounterBloc, CounterState>(
+      builder: (context, state) {
+        if (state is LoadingState) {
+          return LoadingAnimation();
+        } else {
+          return Container(
+              child: Center(
+            child: Column(
+              mainAxisAlignment: MainAxisAlignment.center,
+              children: [
+                Container(
+                  margin: EdgeInsets.only(bottom: 10),
+                  child: FloatingActionButton(
+                    onPressed: () => context
+                        .read<CounterBloc>()
+                        .add(IncrementCounterEvent(counter: state.counter)),
+                    child: Icon(Icons.add),
+                  ),
+                ),
+                CircleAvatar(
+                  child: Text('${state.counter}'),
+                ),
+                Container(
+                  margin: EdgeInsets.only(top: 10),
+                  child: FloatingActionButton(
+                    onPressed: () => context
+                        .read<CounterBloc>()
+                        .add(DecrementCounterEvent(counter: state.counter)),
+                    child: Icon(Icons.remove),
+                  ),
+                )
+              ],
+            ),
+          ));
+        }
+      },
+    );
+  }
+}
+
+class LoadingAnimation extends StatelessWidget {
+  @override
+  Widget build(BuildContext context) {
     return Container(
-        child: Center(
-      child: Column(
-        mainAxisAlignment: MainAxisAlignment.center,
-        children: [
-          Container(
-            margin: EdgeInsets.only(bottom: 10),
-            child: FloatingActionButton(
-              onPressed: () {},
-              child: Icon(Icons.add),
-            ),
-          ),
-          CircleAvatar(
-            child: Text("0"),
-          ),
-          Container(
-            margin: EdgeInsets.only(top: 10),
-            child: FloatingActionButton(
-              onPressed: () {},
-              child: Icon(Icons.remove),
-            ),
-          )
-        ],
+      child: Center(
+        child: CircularProgressIndicator(
+          backgroundColor: Colors.red,
+        ),
       ),
-    ));
+    );
   }
 }
