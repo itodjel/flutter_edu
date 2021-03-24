3a437942de59fc37fee501b69b11c6efb0d5bf5a
luka <luka.jelacic@edu.fit.ba>
2021-03-24 10:53:56 +0100

    added BlocProvider in UI

--- challenges/logic/bloc_counter/luka_jelacic/lib/myscaffold.dart
+++ challenges/logic/bloc_counter/luka_jelacic/lib/myscaffold.dart
@@ -1,18 +1,23 @@
 import 'package:flutter/material.dart';
+import 'package:flutter_bloc/flutter_bloc.dart';
+import 'package:luka_jelacic/blocs/counter_bloc.dart';
 
 import 'mybody.dart';
 
 class MyScaffold extends StatelessWidget {
   @override
   Widget build(BuildContext context) {
-    return SafeArea(
-      child: Scaffold(
-        appBar: AppBar(
-          leading: Icon(Icons.arrow_back_ios_outlined),
-          title: Text("Bloc Logic"),
-          actions: [IconButton(icon: Icon(Icons.favorite), onPressed: () {})],
+    return BlocProvider(
+      create: (context) => CounterBloc(),
+      child: SafeArea(
+        child: Scaffold(
+          appBar: AppBar(
+            leading: Icon(Icons.arrow_back_ios_outlined),
+            title: Text("Bloc Logic"),
+            actions: [IconButton(icon: Icon(Icons.favorite), onPressed: () {})],
+          ),
+          body: MyBody(),
         ),
-        body: MyBody(),
       ),
     );
   }
