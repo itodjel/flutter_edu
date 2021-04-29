c5cb49c35144a0368fde5df833ad15d342f17cd8
Salih Agić <salih.agic@outlook.com>
2021-04-29 13:01:57 +0200

    removed unused icons

--- flutter_boilerplate/lib/presentation/helpers/app_icons.dart
+++ flutter_boilerplate/lib/presentation/helpers/app_icons.dart
@@ -1,16 +1,5 @@
-import 'package:flutter/material.dart';
-import 'package:flutter/widgets.dart';
+import 'package:flutter_boilerplate/all.dart';
 
 class AppIcons {
-  static const IconData profile = Icons.supervised_user_circle;
-  static const IconData more = Icons.more;
-  static const IconData close = Icons.close;
-  static const IconData back = Icons.close;
-  static const IconData check_circle_outline = Icons.check_circle_outline;
-  static const IconData delete = Icons.delete;
-  static const IconData computer = Icons.computer;
-  static const IconData email = Icons.email;
-  static const IconData chevron_down = Icons.arrow_drop_down_outlined;
-  static const IconData chevron_left = Icons.chevron_left;
-  static const IconData confirmation_sheet = Icons.call_to_action;
+  static const IconData home = Icons.home;
 }
