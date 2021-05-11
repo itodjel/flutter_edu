a791bf8d03d4b2684e009fb899b4b6bd8c87fe2f
Salih Agić <salih.agic@outlook.com>
2021-05-11 08:44:44 +0200

    moved enumerations up one folder

--- flutter_boilerplate/lib/domain/all.dart
+++ flutter_boilerplate/lib/domain/all.dart
@@ -1,3 +1,4 @@
+export 'enumerations/all.dart';
 export 'models/all.dart';
 export 'repositories/all.dart';
 export 'validators/all.dart';
--- flutter_boilerplate/lib/domain/models/all.dart
+++ flutter_boilerplate/lib/domain/models/all.dart
@@ -1,4 +1,3 @@
-export 'enumerations/all.dart';
 export 'local/all.dart';
 export 'requests/all.dart';
 export 'responses/all.dart';
