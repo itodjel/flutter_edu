462c316ed7cd8973350c134e1c41a901c3c0f7d1
Salih Agić <salih.agic@outlook.com>
2022-05-17 23:32:50 +0200

    feature: mock auth repo added

--- appito/lib/domain/repositories/authentication_repository.dart
+++ appito/lib/domain/repositories/authentication_repository.dart
@@ -6,6 +6,38 @@ abstract class IAuthenticationRepository {
   Future<Result> signOut();
 }
 
+class MockAuthenticationRepository implements IAuthenticationRepository {
+  final IRestApiClient restApiClient;
+
+  MockAuthenticationRepository({required this.restApiClient});
+
+  @override
+  Future<bool> isAuthenticated() async => restApiClient.authHandler.isAuthorized();
+
+  @override
+  Future<Result> signIn(SignInRequestModel model) async {
+    await Future.delayed(const Duration(seconds: 5));
+
+    await restApiClient.authHandler.authorize(
+      jwt: '<JWT_VALUE_HERE>',
+      refreshToken: '<REFRESH_TOKEN_HERE>',
+    );
+
+    return NetworkResult();
+  }
+
+  @override
+  Future<Result> signOut() async {
+    try {
+      await restApiClient.authHandler.unAuthorize();
+
+      return NetworkResult();
+    } catch (e) {
+      return NetworkResult(exception: Exception(e));
+    }
+  }
+}
+
 class AuthenticationRepository implements IAuthenticationRepository {
   final IRestApiClient restApiClient;
 
--- appito/lib/startup/repositories_configuration.dart
+++ appito/lib/startup/repositories_configuration.dart
@@ -2,6 +2,10 @@ import 'package:appito/_all.dart';
 
 class RepositoriesConfiguration {
   static Future configure() async {
-    services.registerSingleton<IAuthenticationRepository>(AuthenticationRepository(restApiClient: services.get<IRestApiClient>()));
+    // Real repo to use when we want to authenticate via our API
+    // services.registerSingleton<IAuthenticationRepository>(AuthenticationRepository(restApiClient: services.get<IRestApiClient>()));
+
+    // Mock repo to use when there is no API to fake user sign in
+    services.registerSingleton<IAuthenticationRepository>(MockAuthenticationRepository(restApiClient: services.get<IRestApiClient>()));
   }
 }
