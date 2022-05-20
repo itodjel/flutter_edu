a237bdd111ca0a25877243bda67f314d9d56d83d
salihagic <salih.agic@outlook.com>
2022-05-20 09:04:23 +0200

    fix: app navigation fix

--- appito/lib/config/app_settings/development_app_settings.dart
+++ appito/lib/config/app_settings/development_app_settings.dart
@@ -2,5 +2,5 @@ import 'package:appito/_all.dart';
 
 AppSettings developmentAppSettings = AppSettings(
   baseApiUrl: 'https://d70a-92-36-186-127.eu.ngrok.io',
-  resetStorageOnRestart: false,
+  resetStorageOnRestart: true,
 );
